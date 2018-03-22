87d062c8aa520400e0c02fdb31ff37a9e19b07ea
Djorkaeff Alexandre Vilela Pereira <djorkaeff.unb@gmail.com>
2018-03-22 08:54:19 -0300

    Initial project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "no-console": 0,
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "linebreak-style": 0
+    },
+};
